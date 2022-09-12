a85d4f914384435bc79aa9909cf3a22c6d6ad076
Bardia <99704122@mydavinci.nl>
2022-09-12 23:38:47 +0200

    school

---
+++ extra/tijd.py
@@ -0,0 +1,7 @@
+uren = int(input("Hoelaat is het in uren?"))
+minuten = int(input("Hoelaat is het in minuten?"))
+
+antwoord = 23 - uren
+antwoord1 = 60 - minuten
+
+print (f"De dag is voorbij over {antwoord} uur en {antwoord1} minuten")
