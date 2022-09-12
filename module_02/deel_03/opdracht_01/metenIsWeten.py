43401b0fec9a75b369cdd7ef175d50b3e91b8e8a
Bardia <99704122@mydavinci.nl>
2022-09-12 12:12:45 +0200

    Min en Max

--- module_02/deel_03/opdracht_01/metenIsWeten.py
+++ module_02/deel_03/opdracht_01/metenIsWeten.py
@@ -2,11 +2,14 @@ a = int(input("geef een getal:"))
 b = int(input("geef een getal:"))
 
 Max = a
-
+Min = b
 
 if a > b:
-    print(f"a is het grootste getal:{Max}")
+    print(f"a is het grootste getal: {Max}")
+elif a < b:
+    print (f"a is het kleinste getal: {Min}")
+else:
+    print (f"a en b zijn even groot")
 
-
-
-
+print (f"Het minimum is: {Min}")
+print (f"Het maximum is: {Max}")
