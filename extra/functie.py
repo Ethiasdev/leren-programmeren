19843a96af529dd555b8ffcf9a31eb8384439f46
Bardia <99704122@mydavinci.nl>
2022-10-03 09:17:33 +0200

    raadEens

---
+++ extra/functie.py
@@ -0,0 +1,9 @@
+zin = "eten is lekker"
+
+for i in zin:
+    print(i)
+
+print(abs( - 5))
+
+print(101//25) # int deling
+print(101 % 25)# modulo of rest
--- module_03/proef_a_raad_eens/opdracht_02/raadEens.py
+++ module_03/proef_a_raad_eens/opdracht_02/raadEens.py
@@ -1,51 +1,50 @@
 import random
-import sys
 
-pogingen = 0
-rondes = 0
+poging = 0
+ronde = 0
 score = 0
-cijferControle = 0
+AANTAL_RONDES = 20
+AANTAL_POGINGEN = 10
 
 
-while rondes < 20:
-    num = random.randint(1, 1000)
+while ronde < AANTAL_RONDES:
     raden = input("Wil je een getal raden?: ").lower()
-    if raden == "ja":
-        print(num) # dit is om te het testen van de programma makkelijker te maken
-        while pogingen < 10:  # loop van het getal raden stopt na 10 pogingen
-            while cijferControle == 0:
-                try: # verplicten van cijfers
-                    antwoord = int(input("Raad het getal: "))
-                    break
-                except ValueError:
-                    print("Cijfers alleen AUB!")
-
-            if antwoord == num:
-                print("Je hebt het goed geraden!")
-                score += 1
-                break
-            else:
-                if antwoord >= num: # berekening of antwoord groter is dan het nummer daarmee berekent hij de verschil
-                    verschil = antwoord - num
-                elif antwoord <= num:
-                    verschil = num - antwoord
-                if verschil <= 20: # berekent hier of het heel warm is of warm
-                    print("Je bent heel warm!")
-                elif verschil <= 50 and verschil >= 20:
-                    print("Je bent warm!")
-            pogingen += 1
-                    
-    elif raden == "nee":
+    
+    if raden == "nee":
         print("Oke jammer!")
-        print(f"Jou eind score is {score} van de 20 punten!")
+        print(f"Jou eind score is {score} van de {AANTAL_RONDES} punten!")
         exit()
+
+    num = random.randint(1, 1000)
+    print(num) # dit is om te het testen van de programma makkelijker te maken
+    while poging < AANTAL_POGINGEN:  # loop van het getal raden stopt na 10 pogingen
+        while True:
+            try: # verplicten van cijfers
+                antwoord = int(input("Raad het getal: "))
+                break #loop and a half
+            except ValueError:
+                print("Cijfers alleen AUB!")
+
+        if antwoord == num:
+            print("Je hebt het goed geraden!")
+            score += 1
+            break
+        else: # abs gebruiken
+            verschil = abs(antwoord - num)
+            if verschil <= 20: # berekent hier of het heel warm is of warm
+                print("Je bent heel warm!")
+            elif verschil <= 50 and verschil >= 20:
+                print("Je bent warm!")
+        poging += 1
+                
+
     print(f"Je hebt {score} punt(en)!")
-    rondes += 1
-    pogingen = 0
+    ronde += 1
+    poging = 0
 
         
 
-print(f"Jou eind score is {score} van de 20 punten!")
+print(f"Jou eind score is {score} van de {AANTAL_RONDES} punten!")
     
 
 
