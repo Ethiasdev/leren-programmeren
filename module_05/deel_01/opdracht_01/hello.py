d666e5afd4388ea4e120a52af897a66d8005b3d2
Bardia <99704122@mydavinci.nl>
2022-11-14 14:20:48 +0100

    functions

---
+++ module_05/deel_01/opdracht_01/hello.py
@@ -0,0 +1,5 @@
+def my_function(getal):
+    for k in range(getal):
+        print(f"Hello from function town {k + 1}! ")
+
+my_function(7)
---
+++ module_05/deel_01/opdracht_02/multiplier-x.py
@@ -0,0 +1,9 @@
+def my_function(getal):
+    for k in range(10):
+        som = (k + 1) * getal
+        print (f"{getal} x {k + 1} = {som}")
+
+getal = int(input("van welke getal wil je som weten?: "))
+        
+
+my_function(getal)
