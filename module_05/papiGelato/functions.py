2c3f9bed2c2719ed4917575af8f316334699e22b
Bardia <99704122@mydavinci.nl>
2023-02-07 17:29:19 +0100

    school

--- module_05/papiGelato/functions.py
+++ module_05/papiGelato/functions.py
@@ -1,5 +1,6 @@
 WELKOM = "Welkom bij Papi Gelato!"
 IJSJE_VRAAG = "Hoeveel bolletjes wilt u?"
+LITER_VRAAG = "Hoeveel liter wilt u?"
 PRIJS_BOLLETJES = {
     "vanille" : 0.95
 }
@@ -12,7 +13,7 @@ PRIJS_TOPPINGS = {
     "hagelslag": 0.50, "nootjes": 1.00
     }
 
-BESTELLEN = True
+nogBestellen = False
 BESTELLINGEN = []
 
 SOORT_KLANT = ['particulier', 'zakelijk']
@@ -20,56 +21,79 @@ KLANT_ZAKELIJK = False
 PRIJS_PER_LITER = {
     "vanille" : 9.80
 }
+BTW = 6
 
 # vraag en geef aantal bolletjes van het ijsje
-def vraag_aantal_bolletje() -> int:
-    print(IJSJE_VRAAG)
+def vraag_aantal_bolletje(zakelijk: bool) -> int:
+    if zakelijk:
+        print(LITER_VRAAG)
+    else:
+        print(IJSJE_VRAAG)
     while True:
         antwoord = int(input("aantal :"))
         if antwoord > 0:
             return antwoord
         print("Kies een getal groter dan 0")
 
+
+
 # bereken bakje of hoorntje en vraag dit eventueel.
-def get_hoorntje_of_bakje(grootte: int) -> str:
-    if grootte > 3:
-        print("U krijg een bakje")
-        return "bakje"
-    return input("Wilt u een hoortje of een bakje:")
+def get_hoorntje_of_bakje(grootte: int, zakelijk: bool) -> str:
+    if zakelijk:
+        pass
+    else:
+        if grootte > 3:
+            print("U krijg een bakje")
+            return "bakje"
+        return input("Wilt u een hoorntje of een bakje:")
 
 
-def vraag_smaak_bolletje(aantalBolletjes) -> str:
+def vraag_smaak_bolletje(aantalBolletjes, zakelijk) -> str:
     smaken = []
     for i in range(aantalBolletjes):
-        smaak = input(f"Welke smaak wilt u voor bolletje nummer {i+1}? Aardbei, Chocolade of Vanille? ").lower()
+        if zakelijk:
+            smaak = input(f"Welke smaak wilt u voor liter nummer {i+1}? Aardbei, Chocolade of Vanille? ").lower()
+        else:
+            smaak = input(f"Welke smaak wilt u voor bolletje nummer {i+1}? Aardbei, Chocolade of Vanille? ").lower()
         if smaak in SMAKEN:
             smaken.append(smaak.upper())
         else:
-            print("Sorry, dat snap ik niet...")
+            print("Sorry dat is geen optie die we aanbieden...")
             i -= 1
     return smaken
 
     
-def vraag_toppings() -> list:
-    print(f"Beschikbare toppings: {TOPPINGS[0]}, {TOPPINGS[1]}.")
-    toppings = input("Welke toppings wilt u? (scheid met komma's)")
-    return toppings.split(',')
+def vraag_toppings(zakelijk) -> list:
+    if zakelijk == True:
+        pass
+    else:
+        print(f"Beschikbare toppings: {TOPPINGS[0]}, {TOPPINGS[1]}.")
+        toppings = input("Welke toppings wilt u?: ")
+        if toppings in toppings:
+            return toppings.split(',')
+        else:
+            print("Sorry dat is geen optie die we aanbieden...")
+            vraag_toppings()
 
 def bestellingen_toevoegen(ijsje: dict) -> list:
     BESTELLINGEN.append(ijsje)
     return BESTELLINGEN
 
-def check_nog_bestellen(nogBestellen: str) -> bool:
-    if nogBestellen == "ja":
-        BESTELLEN = False
-        return BESTELLEN
+def check_nog_bestellen(nogBestellen: str, zakelijk) -> bool:
+    if zakelijk == True:
+        return True
     else:
-        BESTELLEN = True
-        return BESTELLEN
+        if nogBestellen == 'ja':
+            return False
+        else:
+            return True
 
-def vraag_nog_een_bestellen() -> str:
-    return input("Wilt u nog een bestelling plaatsen? (ja/nee)")
-
+    
+def vraag_nog_een_bestellen(zakelijk) -> str:
+    if zakelijk == False:
+        return input("Wilt u nog een bestelling plaatsen? (ja/nee)")
+    else:
+        pass
 
 # bereken de prijs van het ijsje
 def bereken_prijs(ijsje: dict) -> float:
@@ -83,25 +107,28 @@ def bereken_prijs(ijsje: dict) -> float:
 
 
 # toon het bonnetje
-def toon_bonnetje(ijsjes: list):
+def toon_bonnetje(ijsjes: list, zakelijk: bool):
     totaal_prijs = 0.0
-    print("Bonnetje:")
-    if BESTELLEN == False:
+    return_string = "Bonnetje: \n" 
+    if zakelijk == False:
         for ijsje in ijsjes:
             smaken = ', '.join(ijsje['smaken'])
             toppings = ', '.join(ijsje['toppings'])
             prijs = ijsje['grootte'] * PRIJS_BOLLETJES['vanille'] + sum(PRIJS_TOPPINGS[topping] for topping in ijsje['toppings'])
             prijs += PRIJS_HOORNTJES[ijsje['verpakking']]
             totaal_prijs += prijs
-            print(f"{smaken}   {ijsje['grootte']} x €{PRIJS_BOLLETJES['vanille']} = €{ijsje['grootte'] * PRIJS_BOLLETJES['vanille']}\n{ijsje['verpakking']}    1 x €{ijsje['verpakking']} = €{PRIJS_HOORNTJES[ijsje['verpakking']]}\nTopping   1 x {toppings} = €{PRIJS_TOPPINGS[toppings]}\n---------------------------")                                             
-        print(f"Totaal : €{round(totaal_prijs,2)}")
+            return_string += f"{smaken}   {ijsje['grootte']} x €{PRIJS_BOLLETJES['vanille']} = €{ijsje['grootte'] * PRIJS_BOLLETJES['vanille']}\n{ijsje['verpakking']}    1 x €{ijsje['verpakking']} = €{PRIJS_HOORNTJES[ijsje['verpakking']]}\nTopping   1 x {toppings} = €{PRIJS_TOPPINGS[toppings]}\n---------------------------\n"                                             
+
     else:
         for ijsje in ijsjes:
             smaken = ', '.join(ijsje['smaken'])
             totaal_prijs += ijsje['prijs']
-            print(f"L.{smaken}    {ijsje['grootte']} x €{PRIJS_PER_LITER['vanille']} = €{ijsje['grootte'] * PRIJS_PER_LITER['vanille']}")
-        print(f"Totaal : €{round(totaal_prijs,2)}")
-        print(f"BTW: €{round(totaal_prijs / 100 * 6, 2)}")
+            return_string += f"L.{smaken}    {ijsje['grootte']} x €{PRIJS_PER_LITER['vanille']} = €{ijsje['grootte'] * PRIJS_PER_LITER['vanille']}\n"
+        
+        return_string += f"BTW: €{round(totaal_prijs / 100 * BTW, 2)}\n"
+    return_string += f"Totaal : €{round(totaal_prijs,2)}"
+
+    return return_string
 
 
 def vraag_zakelijk():
@@ -109,17 +136,14 @@ def vraag_zakelijk():
 
 
 def check_zakelijk(klant):
-    if klant == 'particulier':
-        BESTELLEN = False
-        return BESTELLEN
-    else:
-        BESTELLEN = True
-        return BESTELLEN
+    return klant == 'zakelijk'
+
 
 def bereken_prijs_liters(ijsje):
     prijs = 0.0
     prijs += ijsje["grootte"] * PRIJS_PER_LITER['vanille']
     return prijs
+
 
 
 
--- module_05/papiGelato/programma.py
+++ module_05/papiGelato/programma.py
@@ -2,23 +2,20 @@ from functions import *
 ijsje = {}
 print(WELKOM)
 
-BESTELLEN = check_zakelijk(vraag_zakelijk())
-ijsje['grootte'] = vraag_aantal_bolletje()
-ijsje['smaken'] = vraag_smaak_bolletje(ijsje['grootte'])
-ijsje['prijs'] = bereken_prijs_liters(ijsje)
-bestellingen = bestellingen_toevoegen(ijsje)
 
-while BESTELLEN == False:
-    ijsje['grootte'] = vraag_aantal_bolletje()
-    ijsje['smaken'] = vraag_smaak_bolletje(ijsje['grootte'])
-    ijsje['verpakking'] = get_hoorntje_of_bakje(ijsje['grootte'])
-    ijsje['toppings'] = vraag_toppings()
+
+zakelijk = check_zakelijk(vraag_zakelijk())
+while nogBestellen == False:
+    ijsje['grootte'] = vraag_aantal_bolletje(zakelijk)
+    ijsje['smaken'] = vraag_smaak_bolletje(ijsje['grootte'], zakelijk)
+    ijsje['verpakking'] = get_hoorntje_of_bakje(ijsje['grootte'], zakelijk)
+    ijsje['toppings'] = vraag_toppings(zakelijk)
     ijsje['prijs'] = bereken_prijs(ijsje)
     bestellingen = bestellingen_toevoegen(ijsje)
-    nog_een_bestelling = vraag_nog_een_bestellen()
-    BESTELLEN = check_nog_bestellen(nog_een_bestelling)
+    nog_een_bestelling = vraag_nog_een_bestellen(zakelijk)
+    nogBestellen = check_nog_bestellen(nog_een_bestelling, zakelijk)
 
-toon_bonnetje(bestellingen)
+print(toon_bonnetje(bestellingen, zakelijk))
 
 
 
